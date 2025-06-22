c6d62848d4cbd532605dce65ee55fdef59d191ab
Brian Leonardo de Morais <b.leonardo.morais@gmail.com>
2025-06-22 10:47:50 -0300

    adicionado sufixo de url nas rotas de api

--- src/application/server.go
+++ src/application/server.go
@@ -38,13 +38,13 @@ func (ws *WebServer) setupControllers() {
 }
 
 func (ws *WebServer) setupRoutes(e *echo.Echo) {
-	e.GET("/books", ws.bookController.GetAllBooks)
-	e.POST("/books", ws.bookController.AddBook)
-	e.PUT("/books/:id", ws.bookController.UpdateBookById)
-	e.DELETE("/books/:id", ws.bookController.DeleteBookById)
-	e.GET("/authors", ws.authorController.GetAllAuthors)
-	e.POST("/authors", ws.authorController.AddAuthor)
-	e.GET("/orders/:userId", ws.orderController.GetOrdersByUserId)
-	e.POST("/orders", ws.orderController.AddOrder)
-	e.PUT("/orders/:orderId/:status", ws.orderController.UpdateOrderStatus)
+	e.GET("/api/books", ws.bookController.GetAllBooks)
+	e.POST("/api/books", ws.bookController.AddBook)
+	e.PUT("/api/books/:id", ws.bookController.UpdateBookById)
+	e.DELETE("/api/books/:id", ws.bookController.DeleteBookById)
+	e.GET("/api/authors", ws.authorController.GetAllAuthors)
+	e.POST("/api/authors", ws.authorController.AddAuthor)
+	e.GET("/api/orders/:userId", ws.orderController.GetOrdersByUserId)
+	e.POST("/api/orders", ws.orderController.AddOrder)
+	e.PUT("/api/orders/:orderId/:status", ws.orderController.UpdateOrderStatus)
 }
